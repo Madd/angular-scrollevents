96a5b9993024e5af41506d136e0dbb3c22c90a20
Lasse <lasse.soberg@gmail.com>
2013-03-23 00:29:58 +0100

    Create angular-scrollend.js

---
+++ angular-scrollend.js
@@ -0,0 +1,52 @@
+(function(angular) {
+    angular.module('customEvents', [])
+    .directive('ngOnScrollEnd', ['$parse', function($parse) {
+        return function(scope, element, attr) {
+            var fn = $parse(attr['ngOnScrollEnd']);
+            var opts = $parse(attr['ngOnScrollEnd' + 'Opts'])(scope, {});
+    
+            var interval,
+            el = element[0],
+            scrollPosition = {
+                x: 0,
+                y: 0
+            };
+    
+    
+            var bindScroll = function() {
+                element.bind('scroll', function(event) {
+                    scrollPosition.x = el.scrollLeft;
+                    scrollPosition.y = el.scrollTop;
+    
+                    startInterval(event);
+                    unbindScroll();
+                });
+            };
+    
+            var startInterval = function(event) {
+                interval = setInterval(function() {
+                    if(scrollPosition.x == el.scrollLeft && scrollPosition.y == el.scrollTop) {
+                        clearInterval(interval);
+                        bindScroll();
+                        scrollEndTrigger(event);
+                    } else {
+                        scrollPosition.x = el.scrollLeft;
+                        scrollPosition.y = el.scrollTop;
+                    }
+                }, 150);
+            };
+    
+            var unbindScroll = function() {
+                element.unbind('scroll');
+            };
+    
+            var scrollEndTrigger = function(event) {
+                scope.$apply(function() {
+                    fn(scope, {$event: event});
+                });
+            };
+    
+            bindScroll();
+        };
+    }]);
+})(angular);
